98891bf4bb80e54c26c82c9e11df138e44441464
Ilya Golubkov <golubkov@mcad.ru>
2024-05-16 13:14:49 +0600

    Add: multiply programm

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+
+  @R2
+  M=0
+
+(LOOP)
+  @R1
+  D=M
+  @END // check if in R2 0
+  D;JEQ
+
+  @R0
+  D=M
+  @R2
+  M=M+D // add multiply
+
+  @R1
+  M=M-1 //Decrease R1
+
+  @LOOP
+  0;JMP 
+
+
+(END)
+  @END
+  0;JMP
